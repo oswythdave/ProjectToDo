0a6c926b051851551055cc2677a9f654951a2413
Oswyth Dave <131330691+oswythdave@users.noreply.github.com>
2025-07-10 17:25:22 +0800

    Add files via upload

---
+++ windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,24 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "C:\\Users\\HP\\Downloads\\flutter_windows_3.29.0-stable\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "C:\\Users\\HP\\Downloads\\Praktikum\\praktikum" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.0+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=C:\\Users\\HP\\Downloads\\flutter_windows_3.29.0-stable\\flutter"
+  "PROJECT_DIR=C:\\Users\\HP\\Downloads\\Praktikum\\praktikum"
+  "FLUTTER_ROOT=C:\\Users\\HP\\Downloads\\flutter_windows_3.29.0-stable\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=C:\\Users\\HP\\Downloads\\Praktikum\\praktikum\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=C:\\Users\\HP\\Downloads\\Praktikum\\praktikum"
+  "FLUTTER_TARGET=C:\\Users\\HP\\Downloads\\Praktikum\\praktikum\\lib\\main.dart"
+  "DART_DEFINES=RkxVVFRFUl9WRVJTSU9OPTMuMzIuMQ==,RkxVVFRFUl9DSEFOTkVMPXN0YWJsZQ==,RkxVVFRFUl9HSVRfVVJMPWh0dHBzOi8vZ2l0aHViLmNvbS9mbHV0dGVyL2ZsdXR0ZXIuZ2l0,RkxVVFRFUl9GUkFNRVdPUktfUkVWSVNJT049YjI1MzA1YTg4Mw==,RkxVVFRFUl9FTkdJTkVfUkVWSVNJT049MTQyNWU1ZTllYw==,RkxVVFRFUl9EQVJUX1ZFUlNJT049My44LjE="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=C:\\Users\\HP\\Downloads\\Praktikum\\praktikum\\.dart_tool\\package_config.json"
+)
